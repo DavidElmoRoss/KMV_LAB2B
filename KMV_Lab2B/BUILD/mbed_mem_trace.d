3bc6acb28106cfdd8528589c507cc94538259e45
David Elmo Ross <david.elmo.ross@icloud.com>
2024-08-13 22:17:37 -0400

    first commit

---
+++ KMV_Lab2B/BUILD/mbed_mem_trace.d
@@ -0,0 +1,16 @@
+./build/mbed_mem_trace.o: mbed-os\platform\source\mbed_mem_trace.cpp \
+  mbed_config.h mbed-os\platform\mbed_mem_trace.h \
+  mbed-os\platform\SingletonPtr.h mbed-os\platform\mbed_assert.h \
+  mbed-os\platform\mbed_toolchain.h mbed-os\platform\mbed_preprocessor.h \
+  mbed-os\platform\mbed_atomic.h \
+  mbed-os\targets\TARGET_Freescale\TARGET_MCUXpresso_MCUS\TARGET_MCU_K64F\device\cmsis.h \
+  mbed-os\targets\TARGET_Freescale\TARGET_MCUXpresso_MCUS\TARGET_MCU_K64F\device\fsl_device_registers.h \
+  mbed-os\targets\TARGET_Freescale\TARGET_MCUXpresso_MCUS\TARGET_MCU_K64F\device\MK64F12.h \
+  mbed-os\cmsis\TARGET_CORTEX_M\core_cm4.h \
+  mbed-os\targets\TARGET_Freescale\TARGET_MCUXpresso_MCUS\TARGET_MCU_K64F\device\system_MK64F12.h \
+  mbed-os\targets\TARGET_Freescale\TARGET_MCUXpresso_MCUS\TARGET_MCU_K64F\device\MK64F12_features.h \
+  mbed-os\targets\TARGET_Freescale\TARGET_MCUXpresso_MCUS\TARGET_MCU_K64F\device\cmsis_nvic.h \
+  mbed-os\platform\cxxsupport\mstd_type_traits \
+  mbed-os\platform\cxxsupport\mstd_cstddef \
+  mbed-os\platform\internal\mbed_atomic_impl.h \
+  mbed-os\platform\PlatformMutex.h mbed-os\platform\NonCopyable.h
